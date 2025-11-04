b06f7d0b42443200084ec6e8d2c8d3840301fe5d
emanuel-gf <emanuelgoulartf@gmail.com>
2025-11-04 06:23:48 -0300

    Add create_cluster session on the class.

--- src/s3handler.py
+++ src/s3handler.py
@@ -6,19 +6,16 @@ from pyspark.ml import Pipeline
 from pyspark.ml.feature import VectorAssembler, StandardScaler
 import os
 
-## Note
-## This is slightly different from Sparker2.py because there are two methods
-## to create the session, locally and on the bucket, this is more for a 
-## single file exploration but running on a jupyter notebook
 
 class Sparker:
     """
     A class to handle Spark operations on S3 parquet files.
     """
     
-    def __init__(self, access_key, secret_key, run_locally=False):
+    def __init__(self, access_key=None, secret_key=None):
         """
         Initialize Sparker with S3 credentials and file information.
+        Access key and Secret key are only necessary when not running inside the AWS EC2 cluster.
         
         Args:
             access_key (str): AWS access key
@@ -26,35 +23,28 @@ class Sparker:
         """
         self.access_key = access_key
         self.secret_key = secret_key
-        
         self.spark = None
+        self.executor_mem = "4g"
+        self.driver_mem = "4g"
+    
         
-  
-    def _create_session(self):
-        """
-        Create and configure Spark session with S3 settings.
-        
-        Returns:
-            SparkSession: Configured Spark session
-        """
-        spark = SparkSession.builder \
-            .appName("Sparker - S3 Parquet Reader") \
-            .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
-            .config("spark.hadoop.fs.s3a.access.key", self.access_key) \
-            .config("spark.hadoop.fs.s3a.secret.key", self.secret_key) \
-            .config("spark.hadoop.fs.s3a.aws.credentials.provider", 
-                    "org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider") \
-            .config("spark.hadoop.fs.s3a.connection.timeout", "50000") \
-            .config("spark.hadoop.fs.s3a.threads.keepalivetime", "60000") \
-            .config("spark.hadoop.fs.s3a.multipart.purge.age", "30000000") \
-            .config("spark.hadoop.fs.s3a.connection.establish.timeout", "30000") \
-            .getOrCreate()
-
-        spark.sparkContext.setLogLevel("ERROR")
-        
+    def _create_on_cluster_session(self):
+        spark =  ( 
+            SparkSession.builder 
+                .appName("Read FRACTAL files") 
+                .config("spark.hadoop.fs.s3a.fast.upload", "true")
+                .config("spark.hadoop.fs.s3a.multipart.size", "104857600")
+                .config("spark.executor.memory",self.executor_mem)
+                .config("spark.driver.memory", self.driver_mem)
+                .getOrCreate()
+            )
         self.spark = spark
-    
-    
+        
+        print("Session Created!")
+        print(f"- executor memory= {self.executor_mem}")
+        print(f"- driver memory= {self.driver_mem}")
+        
+          
     def _create_local_session(self):
         """
         Try to create a local session to run in a notebook
